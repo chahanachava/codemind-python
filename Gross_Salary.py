69356171a1500daed8bcc9e0a027839159468cbc
chahanachava <142240687+chahanachava@users.noreply.github.com>
2023-08-18 16:01:22 +0530

    b=float(input()) if(b<=10000):     da=b*0.8     hra=b*0.2 elif(b<=20000):     da=b*0.9     hra=b*0.25 elif(b>20000):     da=b*0.95     hra=b*0.3 gs=b+da+hra print(f"{gs:.2f}")

---
+++ Gross_Salary.py
@@ -0,0 +1,12 @@
+b=float(input())
+if(b<=10000):
+    da=b*0.8
+    hra=b*0.2
+elif(b<=20000):
+    da=b*0.9
+    hra=b*0.25
+elif(b>20000):
+    da=b*0.95
+    hra=b*0.3
+gs=b+da+hra
+print(f"{gs:.2f}")
